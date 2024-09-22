75769a17ad8e0f28ecc8a827b2d5ee6ff2a1a342
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-09-23 00:02:15 +0800

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,13 @@
+cd "$HOME/heminetwork"
+cat ~/popm-address.json
+
+# 自动抓取 private_key
+POPM_BTC_PRIVKEY=$(jq -r '.private_key' ~/popm-address.json)
+read -p "检查 https://mempool.space/zh/testnet 上的 sats/vB 值并输入 / Check the sats/vB value on https://mempool.space/zh/testnet and input: " POPM_STATIC_FEE
+
+export POPM_BTC_PRIVKEY=$POPM_BTC_PRIVKEY
+export POPM_STATIC_FEE=$POPM_STATIC_FEE
+export POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public
+
+cd "$HOME/heminetwork"
+
