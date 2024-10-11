088641011b6856c1a17dd763d83d11057254eee6
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-11 14:59:11 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,3 +1,53 @@
+#=====================================================================升级至0.4.4版本============================================================================================
+screen -S hemi -X quit
+URL="https://github.com/hemilabs/heminetwork/releases/download/v0.4.4/heminetwork_v0.4.4_linux_amd64.tar.gz"
+FILENAME="heminetwork_v0.4.3_linux_amd64.tar.gz"
+DIRECTORY="/root/heminetwork_v0.4.3_linux_amd64"
+ADDRESS_FILE="$HOME/popm-address.json"
+BACKUP_FILE="$HOME/popm-address.json.bak"
+
+echo "备份 address.json 文件..."
+if [ -f "$ADDRESS_FILE" ]; then
+    cp "$ADDRESS_FILE" "$BACKUP_FILE"
+    echo "备份完成：$BACKUP_FILE"
+else
+    echo "未找到 address.json 文件，无法备份。"
+fi
+
+echo "正在下载新版本 $FILENAME..."
+wget -q "$URL" -O "$FILENAME"
+
+if [ $? -eq 0 ]; then
+    echo "下载完成。"
+else
+    echo "下载失败。"
+    exit 1
+fi
+
+echo "删除旧版本目录..."
+rm -rf "$DIRECTORY"
+
+echo "正在解压新版本..."
+tar -xzf "$FILENAME" -C /root
+
+if [ $? -eq 0 ]; then
+    echo "解压完成。"
+else
+    echo "解压失败。"
+    exit 1
+fi
+
+echo "删除压缩文件..."
+rm -rf "$FILENAME"
+
+# 恢复 address.json 文件
+if [ -f "$BACKUP_FILE" ]; then
+    cp "$BACKUP_FILE" "$ADDRESS_FILE"
+    echo "恢复 address.json 文件：$ADDRESS_FILE"
+else
+    echo "备份文件不存在，无法恢复。"
+fi
+#===================================================================================================================================================================================
 cd "$HOME/heminetwork"
 cat ~/popm-address.json
 
@@ -10,7 +60,7 @@ export POPM_STATIC_FEE=$POPM_STATIC_FEE
 export POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public
 
 cd "$HOME/heminetwork"
-pkill screen
+# screen -S hemi -X quit
 screen -dmS hemi bash -c "./popmd"
 
 cd ~
