d3aa8b5e79db45366138733fab3bbfe7bf92eab8
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-24 18:17:02 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #===============================================================================================================================================================
 systemctl stop hemi-restart.service
-pkill screen
+screen -S hemi -X quit
 #===================================================================================================================================================================================
 cd "$HOME/heminetwork"
 cat ~/popm-address.json
@@ -14,35 +14,3 @@ export POPM_STATIC_FEE=$POPM_STATIC_FEE
 export POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public
 cd ~/heminetwork
 screen -dmS hemi bash -c "./popmd"
-# # ===================================公共模块===监控screen模块======================================================================
-# cd ~
-# echo '#!/bin/bash
-# while true
-# do
-#   if ! screen -list | grep -q "hemi"; then
-#     cd ~/heminetwork
-#     cat ~/popm-address.json
-#     POPM_BTC_PRIVKEY=$(jq -r '.private_key' ~/popm-address.json)
-#     export POPM_BTC_PRIVKEY=$POPM_BTC_PRIVKEY
-#     export POPM_STATIC_FEE=$POPM_STATIC_FEE
-#     export POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public
-#     screen -dmS hemi bash -c "./popmd"
-#   fi
-#   sleep 10  # 每隔10秒检查一次
-# done' > regular_hemi.sh
-# ##给予执行权限
-# chmod +x regular_hemi.sh
-# # ================================================================================================================================
-# echo ' [Unit]
-# Description=Restart hemi Service
-# After=network.target
-
-# [Service]
-# Type=simple
-# ExecStart=/bin/bash /root/regular_hemi.sh
-
-# [Install]
-# WantedBy=multi-user.target' > /etc/systemd/system/hemi-restart.service
-# sudo systemctl daemon-reload
-# sudo systemctl enable hemi-restart.service
-# sudo systemctl start hemi-restart.service
